1acea271e0e7f4a2613f22d84d1d9a0d5276ecf2
siddjain <s8002.sid@gmail.com>
2017-07-01 19:02:02 +0530

    Upgrading version

--- Vardhman/AboutBox.Designer.cs
+++ Vardhman/AboutBox.Designer.cs
@@ -98,7 +98,7 @@ namespace Vardhman
             this.labelVersion.Name = "labelVersion";
             this.labelVersion.Size = new System.Drawing.Size(271, 17);
             this.labelVersion.TabIndex = 0;
-            this.labelVersion.Text = "Version: 1.10";
+            this.labelVersion.Text = "Version: 1.11";
             this.labelVersion.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // labelCopyright
